9acecab260d256a58aa5ed23e7b9cd014c9899e2
Rameez <sheikhrameez031@gmail.com>
2024-02-25 18:34:42 +0500

    Initial Commit

--- app.js
+++ app.js
@@ -1,72 +1,102 @@
 //Q2
-var msg = "Hello Yamman, would you like to learn some Python today?!";
-console.log(msg);
+// let msg = "Hello Yamman, would you like to learn some Python today?!";
+// console.log(msg);
 //Q3
-var perName = "Yamman sheikh";
-// Convert to lowercase
-var lowercaseName = perName.toLowerCase();
-console.log("Lowercase: ", lowercaseName);
-// Convert to Uppercase
-var uppercaseName = perName.toUpperCase();
-console.log("UPPERCASE: ", uppercaseName);
-// Convert to title
-var titlecaseName = perName.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
-console.log("TitleCase: ", titlecaseName);
+// let perName = "Yamman sheikh";
+// Convert to lowercase::
+// let lowercaseName = perName.toLowerCase();
+// console.log("Lowercase: ", lowercaseName);
+// Convert to Uppercase::
+// let uppercaseName = perName.toUpperCase();
+// console.log("UPPERCASE: ", uppercaseName);
+// Convert to title::
+// let titlecaseName = perName.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+// console.log("TitleCase: ", titlecaseName);
 //Q4
-var famQoute = "Billgates once said,'If You Are Born Poor It’s Not Your Mistake, But If You Die Poor It’s Your Mistake'";
-console.log(famQoute);
+// let famQoute = "Billgates once said,'If You Are Born Poor It’s Not Your Mistake, But If You Die Poor It’s Your Mistake'"
+// console.log(famQoute);
 //Q5
-var famous_person = "BillGates once said!";
-var famous_qoute = "'If You Are Born Poor It’s Not Your Mistake, But If You Die Poor It’s Your Mistake'";
-console.log(famous_person + famous_qoute);
+// let famous_person = "BillGates once said!";
+// let famous_qoute = "'If You Are Born Poor It’s Not Your Mistake, But If You Die Poor It’s Your Mistake'"
+// console.log(famous_person + famous_qoute);
 //Q6
-var nam = "\t \n Rameez \t \n";
-console.log(nam);
+// let nam = "\t \n Rameez \t \n";
+// console.log(nam);
 //Q7
 //Add
-var addNum = 3 + 5;
-console.log("Addition:\t" + addNum);
+// let addNum = 3 + 5;
+// console.log("Addition:\t" + addNum);
 //Sub
-var subNum = 14 - 6;
-console.log("substraction:\t" + subNum);
+// let subNum = 14 - 6;
+// console.log("substraction:\t" + subNum);
 //Multi
-var multiNum = 2 * 4;
-console.log("Multiply\t" + multiNum);
+// let multiNum = 2 * 4;
+// console.log("Multiply\t" + multiNum);
 //Divide
-var divNum = 32 / 4;
-console.log("Divsion:\t" + divNum);
+// let divNum = 32 / 4;
+// console.log("Divsion:\t" + divNum);
 //Q8
-console.log(7 + 1);
-console.log(18 - 10);
-console.log(4 * 2);
-console.log(40 / 5);
+// console.log(7 + 1);
+// console.log(18 - 10);
+// console.log(4 * 2);
+// console.log(40 / 5);
 //Q9
-var favNum = 5;
-var myMsg = "My favourite num is";
-console.log(myMsg + " " + favNum);
+// let favNum = 5;
+// let myMsg = "My favourite num is";
+// console.log(myMsg + " " + favNum);
 //Q10
 //Rameez Ramzy ---- Date 19/Feb/2024
 //i am Learning Typescript using node.js and my programm going to be well.
 //Q11
-var freindsName = ["Rabbi", "Yamman", "Taswwor"];
+// let freindsName = ["Rabbi", "Yamman", "Taswwor"];
 // console.log(freindsName[0]);
 // console.log(freindsName[1]);
 // console.log(freindsName[2]);
-for (var i = 0; i < freindsName.length; i++) {
-    console.log("Freind " + (i + 1) + ": " + freindsName[i]);
-}
+// for (let i = 0; i < freindsName.length; i++) {
+//     console.log("Freind " + (i + 1) + ": " + freindsName[i])
+// }
 //Q12
-for (var i = 0; i < freindsName.length; i++) {
-    console.log("ASSALAM O ALAIKUM ".concat(freindsName[i], "! Have a Great day"));
-}
+// for (let i = 0; i < freindsName.length; i++) {
+//     console.log(`ASSALAM O ALAIKUM ${freindsName[i]}! Have a Great day`)
+// }
 //Q13
-var cars = ["Supra Mk4", "vigo", "porshe", "Rolce Royce"];
-for (var i = 0; i < cars.length; i++) {
-    console.log("I would like to own a ::".concat(cars[i], ":: Car"));
-}
+// let cars = ["Supra Mk4", "vigo", "porshe", "Rolce Royce"];
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(`I would like to own a ::${cars[i]}:: Car`);
+// }
 //Q14
-var dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien"];
-for (var i = 0; i < dinner_guest.length; i++) {
-    console.log("ASSALAM O ALAIKUM ".concat(dinner_guest[i], ", you are invited to join me for dinner. It would be an honor to have your company."));
-}
+// let dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates"];
+// for (let i = 0; i < dinner_guest.length; i++) {
+//     console.log(`ASSALAM O ALAIKUM Mr. ${dinner_guest[i]}, you are invited to join me for dinner. It would be an honor to have your company.\n`);
+// }
 //Q15
+// dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates",];
+// console.log(`Due to some personal reasons Mr.${dinner_guest[2]} will not come in my dinner `);
+// dinner_guest[2] = "Elon Musk";
+// console.log(`New list of my Freinds who are coming to my dinner\n`)
+// for (let i = 0; i < dinner_guest.length; i++) {
+//     console.log(`${i + 1}. ${dinner_guest[i]}`);
+// }
+//Q16
+// dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates",];
+// for (let i = 0; i < dinner_guest.length; i++) {
+//     console.log(`Dear ${dinner_guest[i]}, I would like to invite you to dinner`)
+// }
+// let a = Math.round(Math.random() * (dinner_guest.length - 1))
+// console.log(`${dinner_guest[a]} can't make it`)
+// dinner_guest.splice(a, a, "Rehmat")
+// console.log(`Dear ${dinner_guest[a]}, I would like to invite you to dinner`)
+// console.log("I have found bigger table")
+// dinner_guest.unshift("Amir")
+// dinner_guest.splice(3, 0, "Baber")
+// dinner_guest.push("Abdullah")
+// console.log(dinner_guest)
+// for (let i = 0; i < dinner_guest.length; i++) {
+//     console.log(`Dear ${dinner_guest[i]}, I would like to invite you to dinner`)
+// }
+var dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates",];
+console.log("".concat(dinner_guest, ":\n I have found bigger dinner table than i think to invite three more freinds"));
+//adding three more freinds 
+//at beginning;
+dinner_guest.unshift("\n \t Elon Musk");
+console.log(dinner_guest.toString());
--- app.ts
+++ app.ts
@@ -4,83 +4,121 @@ console.log(msg);
 
 //Q3
 let perName = "Yamman sheikh";
-// Convert to lowercase
+
+//Convert to lowercase::
 let lowercaseName = perName.toLowerCase();
 console.log("Lowercase: ", lowercaseName);
-// Convert to Uppercase
+
+//Convert to Uppercase::
 let uppercaseName = perName.toUpperCase();
 console.log("UPPERCASE: ", uppercaseName);
-// Convert to title
+
+// Convert to title::
 let titlecaseName = perName.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 console.log("TitleCase: ", titlecaseName);
 
-//Q4
+// Q4
 let famQoute = "Billgates once said,'If You Are Born Poor It’s Not Your Mistake, But If You Die Poor It’s Your Mistake'"
 console.log(famQoute);
 
-//Q5
+// Q5
 let famous_person = "BillGates once said!";
 let famous_qoute = "'If You Are Born Poor It’s Not Your Mistake, But If You Die Poor It’s Your Mistake'"
 console.log(famous_person + famous_qoute);
 
-//Q6
+// Q6
 let nam = "\t \n Rameez \t \n";
 console.log(nam);
 
-//Q7
-//Add
+// Q7
+// Add
 let addNum = 3 + 5;
 console.log("Addition:\t" + addNum);
-//Sub
+
+// Sub
 let subNum = 14 - 6;
 console.log("substraction:\t" + subNum);
-//Multi
+
+// Multi
 let multiNum = 2 * 4;
 console.log("Multiply\t" + multiNum);
-//Divide
+
+// Divide
 let divNum = 32 / 4;
 console.log("Divsion:\t" + divNum);
 
-//Q8
-console.log(7 + 1);
-console.log(18 - 10);
-console.log(4 * 2);
-console.log(40 / 5);
+// Q8
+// console.log(7 + 1);
+// console.log(18 - 10);
+// console.log(4 * 2);
+// console.log(40 / 5);
 
-//Q9
+// Q9
 let favNum = 5;
 let myMsg = "My favourite num is";
 console.log(myMsg + " " + favNum);
 
-//Q10
-//Rameez Ramzy ---- Date 19/Feb/2024
-//i am Learning Typescript using node.js and my programm going to be well.
+// Q10
+/*Rameez Ramzy ---- Date 19/Feb/2024
+i am Learning Typescript using node.js and my programm going to be well.*/
 
-//Q11
+// Q11
 let freindsName = ["Rabbi", "Yamman", "Taswwor"];
-// console.log(freindsName[0]);
-// console.log(freindsName[1]);
-// console.log(freindsName[2]);
+console.log(freindsName[0]);
+console.log(freindsName[1]);
+console.log(freindsName[2]);
 for (let i = 0; i < freindsName.length; i++) {
     console.log("Freind " + (i + 1) + ": " + freindsName[i])
 }
 
-//Q12
+// Q12
 for (let i = 0; i < freindsName.length; i++) {
     console.log(`ASSALAM O ALAIKUM ${freindsName[i]}! Have a Great day`)
 }
 
-//Q13
+// Q13
 let cars = ["Supra Mk4", "vigo", "porshe", "Rolce Royce"];
 for (let i = 0; i < cars.length; i++) {
     console.log(`I would like to own a ::${cars[i]}:: Car`);
 }
 
-//Q14
-let dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien"];
+// Q14
+let dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates"];
 for (let i = 0; i < dinner_guest.length; i++) {
-    console.log(`ASSALAM O ALAIKUM ${dinner_guest[i]}, you are invited to join me for dinner. It would be an honor to have your company.`);
+    console.log(`ASSALAM O ALAIKUM Mr. ${dinner_guest[i]}, you are invited to join me for dinner. It would be an honor to have your company.\n`);
 }
 
-//Q15
+// Q15
+dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates",];
+console.log(`Due to some personal reasons Mr.${dinner_guest[2]} will not come in my dinner `);
+dinner_guest[2] = "Elon Musk";
 
+console.log(`New list of my Freinds who are coming to my dinner\n`)
+for (let i = 0; i < dinner_guest.length; i++) {
+    console.log(`${i + 1}. ${dinner_guest[i]}`);
+}
+
+// Q16
+// dinner_guest = ["Brenden Erich", "Sam Altmen", "Elbert ainstien", "BillGates",];
+// for (let i = 0; i < dinner_guest.length; i++) {
+//     console.log(`Dear ${dinner_guest[i]}, I would like to invite you to dinner`)
+// }
+// let a = Math.round(Math.random() * (dinner_guest.length - 1))
+// console.log(`${dinner_guest[a]} can't make it`)
+// dinner_guest.splice(a, a, "Rehmat")
+// console.log(`Dear ${dinner_guest[a]}, I would like to invite you to dinner`)
+// console.log("I have found bigger table")
+// dinner_guest.unshift("Amir")
+// dinner_guest.splice(3, 0, "Baber")
+// dinner_guest.push("Abdullah")
+// console.log(dinner_guest)
+// for (let i = 0; i < dinner_guest.length; i++) {
+//     console.log(`Dear ${dinner_guest[i]}, I would like to invite you to dinner`)
+// }
+
+console.log(`${dinner_guest}:\n I have found bigger dinner table than i think to invite three more freinds`);
+
+//adding three more freinds 
+//at beginning;
+dinner_guest.unshift("\n \t Elon Musk");
+console.log(dinner_guest.toString());
